b50846a5a7599d1b1c6bbeb20a313170ca84e1d9
Venkat Vedanarayanan <92353275+venkatchennai855@users.noreply.github.com>
2025-01-07 11:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (26.15%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,16 @@
-def steps(n,dp):
-    if(n==1):
-        return 1
-    if(n==2):
-        return 2
-    if(dp[n]!=0):
-        return dp[n]
-    dp[n]=steps(n-1,dp)+steps(n-2,dp)
-    return dp[n]
 class Solution:
+    def solvedp(self,n,dp):
+        if n==1:
+            return 1
+        if n==0:
+            return 1
+        if dp[n]!=0:
+            return dp[n]
+        dp[n]=self.solvedp(n-1,dp)+self.solvedp(n-2,dp)
+        return dp[n]
+
+        return self.solvedp(n-1,dp)+self.solvedp(n-2,dp)
     def climbStairs(self, n: int) -> int:
         dp=[0 for i in range(n+1)]
-        return steps(n,dp)
+        x = self.solvedp(n,dp)
+        return x
